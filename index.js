f6bc3c076e2506fe122d5293654154e6c69cdd13
ixyhoo <ixyhoo@outlook.fr>
2023-06-01 14:02:34 +0200

    convert to input format

--- index.js
+++ index.js
@@ -5,9 +5,21 @@ start_date.min = today;
 
 // Tomorrow date calc
 
+let tomorrow = new Date();
+tomorrow.setDate(tomorrow.getDate() + 1);
 
+//convert to input format
+let tomorrowFormat = tomorrow.toISOString().split('T')[0];
+console.log(tomorrowFormat);
+end_date.value = tomorrowFormat;
+end_date.min = tomorrowFormat;
 
+start_date.addEventListener('change', (e) => {
 
-
-
+    let day = new Date(e.target.value);
+    if (end_date.value < start_date.value) {
+        day.setDate(day.getDate() + 1);
+        end_date.value = day.toISOString().split('T')[0];
+    }
+});
 
